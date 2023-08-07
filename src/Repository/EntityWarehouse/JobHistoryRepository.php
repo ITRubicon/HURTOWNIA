a6718bba0e661d0579d5e4d7996bcdf530162f0c
ANDREW <guzowski.andrew@gmail.com>
2023-08-07 13:04:36 +0200

    uproszczenie

--- src/Repository/EntityWarehouse/JobHistoryRepository.php
+++ src/Repository/EntityWarehouse/JobHistoryRepository.php
@@ -3,97 +3,52 @@
 namespace App\Repository\EntityWarehouse;
 
 use App\EntityWarehouse\JobHistory;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<JobHistory>
- *
- * @method JobHistory|null find($id, $lockMode = null, $lockVersion = null)
- * @method JobHistory|null findOneBy(array $criteria, array $orderBy = null)
- * @method JobHistory[]    findAll()
- * @method JobHistory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class JobHistoryRepository
 {
-    private $em = null;
-    private $qb = null;
+    private $entityManager = null;
 
     public function __construct(ManagerRegistry $registry)
     {
-        $this->em = $registry->getManager('warehouse');
-        $this->qb = new QueryBuilder($this->em);
+        $this->entityManager = $registry->getManager('warehouse');
     }
 
     public function save(JobHistory $entity, bool $flush = false)
     {
-        $this->em->persist($entity);
+        $this->entityManager->persist($entity);
 
         if ($flush)
-            $this->em->flush();
+            $this->entityManager->flush();
 
         return $entity->getId();
     }
 
     public function remove(JobHistory $entity, bool $flush = false): void
     {
-        $this->em->remove($entity);
+        $this->entityManager->remove($entity);
 
         if ($flush)
-            $this->em->flush();
+            $this->entityManager->flush();
     }
 
     public function setEnd(int $id)
     {
-        $this->qb
-            ->update(JobHistory::class, 'j')
-            ->set('j.end', ':time')
-            ->set('j.status', ':status')
-            ->where('j.id = :id')
-            ->setParameter('time', new \DateTime())
+        $dql = "UPDATE App\EntityWarehouse\JobHistory j SET j.end = :end, j.status = :status WHERE j.id = :id";
+        $this->entityManager->createQuery($dql)
             ->setParameter('id', $id)
+            ->setParameter('end', new \DateTime())
             ->setParameter('status', 1)
-            ->getQuery()
             ->execute();
     }
 
     public function setError(int $id, string $msg)
     {
-        $this->qb
-            ->update(JobHistory::class, 'j')
-            ->set('j.error', ':error')
-            ->set('j.status', ':status')
-            ->where('j.id = :id')
-            ->setParameter('error', $msg)
+        $dql = "UPDATE App\EntityWarehouse\JobHistory j SET j.error = :error, j.end = :end, j.status = :status WHERE j.id = :id";
+        $this->entityManager->createQuery($dql)
             ->setParameter('id', $id)
+            ->setParameter('error', $msg)
             ->setParameter('status', -1)
-            ->getQuery()
             ->execute();
     }
-
-//    /**
-//     * @return JobHistory[] Returns an array of JobHistory objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('j')
-//            ->andWhere('j.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('j.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?JobHistory
-//    {
-//        return $this->createQueryBuilder('j')
-//            ->andWhere('j.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
