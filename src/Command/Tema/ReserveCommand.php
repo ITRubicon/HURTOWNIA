d4a3ea3bd6662436630d2a93fb29e351b0666aeb
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2023-09-12 14:05:16 +0200

    pobieranie rezerw

---
+++ src/Command/Tema/ReserveCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Command\Tema;
+
+use App\Command\BaseApiCommand;
+use App\Entity\IConnection;
+use App\Repository\ApiFetchErrorRepository;
+use App\Repository\SourceAuthRepository;
+use App\Repository\Tema\ReserveRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'tema:reserve',
+    description: 'Pobiera listę samochodów. Parametry: nazwa api (opcja)',
+)]
+class ReserveCommand extends BaseApiCommand
+{
+    private $repo;
+    protected $producerName = 'Tema';
+
+    public function __construct(ReserveRepository $repo, SourceAuthRepository $apiAuthRepo, ApiFetchErrorRepository $errorRepo)
+    {
+        parent::__construct($apiAuthRepo, $errorRepo);
+        $this->repo = $repo;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('api', InputArgument::OPTIONAL, 'Nazwa api (ALL lub brak nazwy dla wszystkich)');
+        ;
+    }
+
+    protected function fetch(IConnection $api, SymfonyStyle &$io)
+    {
+        $this->repo->setSource($api);
+        $fetchedRows = $this->repo->fetch();
+        $io->info(sprintf("Pobrano %s rekordów", $fetchedRows['fetched']));
+    }
+
+    protected function clearTable()
+    {
+        $this->repo->clearTable();
+    }
+}
