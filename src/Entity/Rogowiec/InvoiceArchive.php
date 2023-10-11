bbe18cd64551b7278f891f924ff75461d61a5ed8
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2023-10-11 06:44:35 +0200

    archiwum faktur z powodu długiego pobierania

---
+++ src/Entity/Rogowiec/InvoiceArchive.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity\Rogowiec;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Index;
+use Doctrine\ORM\Mapping\Table;
+
+#[ORM\Entity(repositoryClass: CustomerRepository::class)]
+#[Table(name: 'rogowiec_invoice_archive')]
+#[Index(name: "source_idx", fields: ["source", "number"])]
+#[Index(name: "source_id_idx", fields: ["source", "id"])]
+class InvoiceArchive
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $source = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $number = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $correctedNo = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $docDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $saleDate = null;
+
+    #[ORM\Column(type: 'string', length: 30)]
+    private ?string $currency = null;
+
+    #[ORM\Column(type: 'decimal', precision: 12, scale: 2)]
+    private ?string $netValue = null;
+
+    #[ORM\Column(type: 'decimal', precision: 12, scale: 2)]
+    private ?string $grossValue = null;
+}
