3a2d7737cc6a4e8eb0377693b29bcc8e6b12aa64
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2023-12-01 07:58:58 +0100

    bug fix

--- src/Entity/Rogowiec/InvoiceArchive.php
+++ src/Entity/Rogowiec/InvoiceArchive.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping\UniqueConstraint;
 #[Table(name: 'rogowiec_invoice_archive')]
 #[Index(name: "source_idx", fields: ["source", "number"])]
 // #[Index(name: "source_id_idx", fields: ["source", "id"])]
-#[Index("source_id_idx", columns: ["source", "id"])]
+#[Index(name: "source_id_idx", fields: ["source", "id"])]
 #[UniqueConstraint("source_number_un", columns: ["source", "number"])]
 class InvoiceArchive
 {
