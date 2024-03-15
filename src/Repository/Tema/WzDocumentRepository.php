fbda0a7872190c43c137c4b206652c01c94f3fe1
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2024-03-15 10:21:24 +0100

    usunięte ograniczenie z WZ dla stock_id=11

--- src/Repository/Tema/WzDocumentRepository.php
+++ src/Repository/Tema/WzDocumentRepository.php
@@ -96,7 +96,7 @@ class WzDocumentRepository extends IApiRepository
 
     private function getStocks()
     {
-        $q = "SELECT stock_id FROM tema_stock WHERE source = :source and stock_id != 11";
+        $q = "SELECT stock_id FROM tema_stock WHERE source = :source";
         return $this->db->fetchFirstColumn($q, ['source' => $this->source->getName()], ['source' => ParameterType::STRING]);
     }
 
