18c8a04f8f2983caac437409c2e23120ff46822d
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2024-09-03 07:28:12 +0200

    usunięte zbędne komentarze

--- src/Command/Tema/ServiceOrderDocumentCommand.php
+++ src/Command/Tema/ServiceOrderDocumentCommand.php
@@ -6,15 +6,15 @@ use App\Command\BaseApiCommand;
 use App\Entity\IConnection;
 use App\Repository\ApiFetchErrorRepository;
 use App\Repository\SourceAuthRepository;
-use App\Repository\Tema\PackageItemFvRepository;
-use App\Repository\Tema\PackageItemMechanicRepository;
+/* use App\Repository\Tema\PackageItemFvRepository;
+use App\Repository\Tema\PackageItemMechanicRepository; */
 use App\Repository\Tema\ServiceOrderCarRepository;
 use App\Repository\Tema\ServiceOrderDocumentRepository;
 use App\Repository\Tema\ServiceOrderEndDocumentRepository;
 use App\Repository\Tema\ServiceOrderItemInvoiceRepository;
-use App\Repository\Tema\ServiceOrderItemPackageItemRepository;
+// use App\Repository\Tema\ServiceOrderItemPackageItemRepository;
 use App\Repository\Tema\ServiceOrderItemRepository;
-use App\Repository\Tema\ServiceOrderMechanicRepository;
+// use App\Repository\Tema\ServiceOrderMechanicRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -42,10 +42,6 @@ class ServiceOrderDocumentCommand extends BaseApiCommand
         ServiceOrderItemInvoiceRepository $itemInvoiceRepo,
         ServiceOrderEndDocumentRepository $endDocRepo,
         ServiceOrderCarRepository $carRepo,
-        ServiceOrderMechanicRepository $mechanicRepo,
-        ServiceOrderItemPackageItemRepository $packageRepo,
-        PackageItemFvRepository $packageItemFv,
-        PackageItemMechanicRepository $packageItemMechanic,
         SourceAuthRepository $apiAuthRepo,
         ApiFetchErrorRepository $errorRepo)
     {
@@ -55,10 +51,6 @@ class ServiceOrderDocumentCommand extends BaseApiCommand
         $this->endDocRepo = $endDocRepo;
         $this->carRepo = $carRepo;
         $this->itemInvoiceRepo = $itemInvoiceRepo;
-        $this->mechanicRepo = $mechanicRepo;
-        $this->packageRepo = $packageRepo;
-        $this->packageItemFv = $packageItemFv;
-        $this->packageItemMechanic = $packageItemMechanic;
     }
 
     protected function configure(): void
@@ -89,24 +81,6 @@ class ServiceOrderDocumentCommand extends BaseApiCommand
 
             $result = $this->itemRepo->saveItems($fetchedRows['items']);
             $io->info(sprintf('Zapisano %s pozycji z faktur', $result['fetched']));
-/*             
-            $res = $this->itemInvoiceRepo->saveInvoices($result['invoices']);
-            $io->info(sprintf('Zapisano %s faktur przypisanych do pozycji zlecenia', $res));
-            unset($result['invoices']);
-
-            $res = $this->mechanicRepo->saveMechanics($result['workerHours']);
-            $io->info(sprintf('Zapisano %s mechaników', $res));
-            unset($result['workerHours']);
-            
-            $result = $this->packageRepo->saveItems($result['packageItems']);
-            $io->info(sprintf('Zapisano %s pozycji z pakietów', $result['fetched']));
-
-            $res = $this->packageItemFv->saveInvoices($result['invoices']);
-            $io->info(sprintf('Zapisano %s faktur z pakietów', $res));
-            
-            $res = $this->packageItemMechanic->saveMechanics($result['workerHours']);
-            $io->info(sprintf('Zapisano %s mechaników z pakietów', $res));
- */
             unset($result);
         }
         unset($fetchedRows['items']);
