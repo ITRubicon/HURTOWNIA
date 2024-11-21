c9a834151bca541e165e4d9012ae005de0064e04
ANDREW <andrzej.guzowski@rubicon.katowice.pl>
2024-11-21 07:43:13 +0100

    pobieranie faktur zakupowych i korekt zakupu z Rogowca

---
+++ src/Entity/Rogowiec/CarInvoices.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity\Rogowiec;
+
+use App\Repository\Rogowiec\CarsSoldRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Index;
+use Doctrine\ORM\Mapping\Table;
+
+#[ORM\Entity(repositoryClass: CarsSoldRepository::class)]
+#[Table(name: 'rogowiec_car_invoices')]
+#[Index(name: "source_idx", fields: ["source"])]
+class CarInvoices
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $source = null;
+
+    #[ORM\Column]
+    private ?int $vehicle_id = null;
+
+    #[ORM\Column(length: 17)]
+    private ?string $vin = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $fv_numer = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fv_data = null;
+    
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fv_data_wplyw = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 12, scale: 2)]
+    private ?string $wartosc = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $faktura_rodzaj = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $kod_typu_korekty = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $typ_korekty = null;
+}
---
+++ src/Repository/Rogowiec/CarsInvoicesRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Repository\Rogowiec;
+
+use App\Repository\IApiRepository;
+use Doctrine\DBAL\ParameterType;
+
+class CarsInvoicesRepository extends IApiRepository
+{
+    private $endpoint = '/wdf/Reports/vehicleInvoiceList?VIN={vin}&id_oddzial={branch_id}';
+    protected $table = 'rogowiec_car_invoices';
+
+    public function fetch(): array
+    {
+        $this->clearDataArrays();
+        $vinBranches = $this->fetchBranchWithVin();
+        $vinCount = count($vinBranches);
+        $resCount = 0;
+
+        if ($vinCount) {
+            $i = 1;
+
+            foreach ($vinBranches as $vb) {
+                echo "\nId jednostki {$vb['branch_id']}, VIN: {$vb['vin']} ----> $i/$vinCount";
+                $url = str_replace(
+                    ['{vin}', '{branch_id}'],
+                    [$vb['vin'], $vb['branch_id']],
+                    $this->endpoint
+                );
+                $this->fetchResult = $this->fetchApiResult($url);
+                $resCount += count($this->fetchResult);
+                $this->save();
+                $this->clearDataArrays();
+                $i++;
+            }
+        } else
+            throw new \Exception("Nie żadnych oddziałów lub VINów dla " . $this->source->getName() . ". Najpierw uruchom komendę pobierającą listę oddziałów [rogowiec:branch] i sprzedanych samochodów [rogowiec:cars:sold]", 99);
+
+        return ['fetched' => $resCount];
+    }
+
+    private function fetchBranchWithVin()
+    {
+        $q = "SELECT
+                b.source,
+                b.dms_id AS branch_id,
+                c.vin
+            FROM prehurtownia.rogowiec_branch b
+            JOIN prehurtownia.rogowiec_cars_sold c ON c.source = b.source AND SUBSTRING(c.fv_numer, 1, 1) = CAST(b.oddzial_id AS UNSIGNED)
+            WHERE b.source = :source
+        ";
+        return $this->db->fetchAllAssociative($q, ['source' => $this->source->getName()], ['source' => ParameterType::STRING]);
+    }
+
+    protected function getFieldsParams(): array
+    {
+        return [
+            'vehicle_id' => ['sourceField' => 'VehicleId', 'type' => ParameterType::STRING],
+            'vin' => ['sourceField' => 'VIN', 'type' => ParameterType::STRING],
+            'fv_numer' => ['sourceField' => 'NumerFaktury', 'type' => ParameterType::STRING],
+            'fv_data' => ['sourceField' => 'DataWystawieniaFaktury', 'type' => ParameterType::STRING, 'format' => ['date' => 'Y-m-d']],
+            'fv_data_wplyw' => ['sourceField' => 'DataWplywuFaktury', 'type' => ParameterType::STRING, 'format' => ['date' => 'Y-m-d']],
+            'wartosc' => ['sourceField' => 'WartoscFaktury', 'type' => ParameterType::STRING],
+            'faktura_rodzaj' => ['sourceField' => 'RodzajFaktury', 'type' => ParameterType::STRING],
+            'kod_typu_korekty' => ['sourceField' => 'KodTypuKorekty', 'type' => ParameterType::STRING],
+            'typ_korekty' => ['sourceField' => 'TypKorekty', 'type' => ParameterType::STRING],
+            'source' => ['sourceField' => 'source', 'type' => ParameterType::STRING],
+        ];
+    }
+}
